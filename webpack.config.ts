29b5ea629862618b9fb48a01d50c90a43bd1aad6
Umuril <umuril.lyerood@gmail.com>
2017-06-02 14:11:36 +0200

    Aurelia plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -21,6 +21,8 @@ import * as copyFiles from '@easy-webpack/config-copy-files';
 import * as uglify from '@easy-webpack/config-uglify';
 import * as generateCoverage from '@easy-webpack/config-test-coverage-istanbul';
 
+import * as AureliaWebpackPlugin from 'aurelia-webpack-plugin';
+
 const ENV: 'development' | 'production' | 'test' = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() || (process.env.NODE_ENV = 'development');
 
 // basic configuration:
@@ -29,6 +31,8 @@ const baseUrl = '/';
 const rootDir = path.resolve();
 const srcDir = path.resolve('src');
 const outDir = path.resolve('dist');
+
+const allOptions = { root: rootDir, src: srcDir, title: title, baseUrl: baseUrl };
 
 const coreBundles = {
   bootstrap: [
@@ -79,39 +83,53 @@ let config = generateConfig(
     output: {
       path: outDir,
     },
+    resolve: {
+      modules: [
+        srcDir,
+        'node_modules'
+      ],
+    },
+    plugins: [
+      new AureliaWebpackPlugin(allOptions)
+    ],
+    metadata: {
+      title,
+      baseUrl,
+      rootDir,
+      srcDir,
+    }
   },
 
   /**
    * Don't be afraid, you can put bits of standard Webpack configuration here
    * (or at the end, after the last parameter, so it won't get overwritten by the presets)
    * Because that's all easy-webpack configs are - snippets of premade, maintained configuration parts!
-   * 
+   *
    * For Webpack docs, see: https://webpack.js.org/configuration/
    */
 
-  ENV === 'test' || ENV === 'development' ? 
-    envDev(ENV !== 'test' ? {} : {devtool: 'inline-source-map'}) :
+  ENV === 'test' || ENV === 'development' ?
+    envDev(ENV !== 'test' ? {} : { devtool: 'inline-source-map' }) :
     envProd({ /* devtool: '...' */ }),
 
-  aurelia({root: rootDir, src: srcDir, title: title, baseUrl: baseUrl}),
   typescript(ENV !== 'test' ? {} : { options: { doTypeCheck: false, sourceMap: false, inlineSourceMap: true, inlineSources: true } }),
   html(),
   css({ filename: 'styles.css', allChunks: true, sourceMap: false }),
   fontAndImages(),
   globalBluebird(),
   globalJquery(),
-  generateIndexHtml({minify: ENV === 'production'}),
+  generateIndexHtml({ minify: ENV === 'production' }),
 
   ...(ENV === 'production' || ENV === 'development' ? [
-      commonChunksOptimize({appChunkName: 'app', firstChunk: 'aurelia-bootstrap'}),
-      copyFiles({patterns: [{ from: 'favicon.ico', to: 'favicon.ico' }]})
-    ] : [
-    /* ENV === 'test' */
-    generateCoverage({ options: { esModules: true } })
-  ]),
+    commonChunksOptimize({ appChunkName: 'app', firstChunk: 'aurelia-bootstrap' }),
+    copyFiles({ patterns: [{ from: 'favicon.ico', to: 'favicon.ico' }] })
+  ] : [
+      /* ENV === 'test' */
+      generateCoverage({ options: { esModules: true } })
+    ]),
 
   ENV === 'production' ?
-    uglify({debug: false, mangle: { except: ['cb', '__webpack_require__'] }}) : {}
+    uglify({ debug: false, mangle: { except: ['cb', '__webpack_require__'] } }) : {}
 );
 
 module.exports = stripMetadata(config);
